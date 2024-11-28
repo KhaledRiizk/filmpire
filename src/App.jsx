a9e91e9d7bf1d3a9afb4b3e4ce6160ba5956af5f
Khaled Rizk <kh413d413@gmail.com>
2024-11-28 17:21:26 +0200

    file and folder structure

---
+++ src/App.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import { CssBaseline } from "@mui/material";
+import { Route, Switch } from "react-router-dom";
+import Movies from "./components/Movies/Movies";
+import Profile from "./components/Profile/Profile";
+import MovieInformation from "./components/MovieInformation/MovieInformation";
+import Actors from "./components/Actors/Actors";
+import NavBar from "./components/NavBar/NavBar";
+import useStyles from './styles'
+
+const App = () => {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <CssBaseline />
+      <NavBar />
+      <main className={classes.content}>
+        <div className={classes.toolbar}/>
+        <Switch>
+          <Route exact path="/">
+            <Movies />
+          </Route>
+          <Route exact path="/profile/:id">
+            <Profile />
+          </Route>
+          <Route exact path="/movie/:id">
+            <MovieInformation />
+          </Route>
+          <Route exact path="/actors/:id">
+            <Actors />
+          </Route>
+        </Switch>
+      </main>
+    </div>
+  );
+};
+
+export default App;

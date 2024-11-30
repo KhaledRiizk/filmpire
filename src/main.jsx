ae503d4efb736bf4eebbdc87eeb41e1582f575ad
Khaled Rizk <kh413d413@gmail.com>
2024-11-30 16:22:53 +0200

    setup redux. BUG: process not defined

--- src/main.jsx
+++ src/main.jsx
@@ -2,16 +2,20 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom/cjs/react-router-dom.js";
 import { createTheme, ThemeProvider } from "@mui/material";
+import { Provider } from "react-redux";
+import store from "./app/store.jsx";
 import App from "./App.jsx";
 
 const theme = createTheme({});
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <ThemeProvider theme={theme}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </ThemeProvider>
+    <Provider store={store}>
+      <ThemeProvider theme={theme}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </ThemeProvider>
+    </Provider>
   </StrictMode>
 );

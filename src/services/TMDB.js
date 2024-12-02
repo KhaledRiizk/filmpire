af7ff92d75ce19356295802abab8fae0c9f4aebe
Khaled Rizk <kh413d413@gmail.com>
2024-12-02 10:16:45 +0200

    Finalize search functionality

--- src/services/TMDB.js
+++ src/services/TMDB.js
@@ -15,7 +15,12 @@ export const tmdbApi = createApi({
 
     // Get Movies by [Type]
     getMovies: builder.query({
-      query: ({ genreIdOrCategoryName, page }) => {
+      query: ({ genreIdOrCategoryName, page, searchQuery }) => {
+        // Get movies by search
+        if (searchQuery) {
+          return `search/movie?query=${searchQuery}&page=${page}&api_key=${tmdbApiKey}`;
+        }
+
         // Get movies by categories
         if (
           genreIdOrCategoryName &&
